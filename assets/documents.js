d97351dbc382165e38d58e01bd8ff83a3dc7277e
Fleisar <48710716+Fleisar@users.noreply.github.com>
2021-04-17 13:12:41 +0700

    decorations and optimization
    mobile adaptive
    image poster on top
    add css global color

--- assets/documents.js
+++ assets/documents.js
@@ -1,7 +1,22 @@
+let asyncImg;
 $(function(){
+    asyncImg = $(".asyncImage");
+    asyncImg.map((index) => {
+        let eIMG = $(".asyncImage").eq(index);
+        eIMG.removeClass(".asyncImage");
+        let qualities = JSON.parse(eIMG.attr("qualities"));
+        eIMG.removeAttr("qualities");
+        for(let i = 0; i < qualities.length; i++){
+            let image = new Image();
+            image.src = qualities[i];
+            image.onload = () => {
+                eIMG.attr("src", qualities[i]);
+            }
+        }
+    });
     $('a[scroll]').click(function(e){
         if(!$(this).attr('scroll').startsWith('#')) return 0
-        $('html').stop().animate({scrollTop:$($(this).attr('scroll')).offset().top-70})
+        $('html').stop().animate({scrollTop:$($(this).attr('scroll')).offset().top-100})
         return e.preventDefault()
     })
 })
